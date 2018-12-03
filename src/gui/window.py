a1e3b38c60ec0bc6d0bf4ad5477cda76112b608b
Ignacio Marroquin <ignacio9295@gmail.com>
2018-12-02 19:37:51 -0600

    Added a simple window
    Simple window with two buttons: Greet and Exit

--- src/gui/window.py
+++ src/gui/window.py
@@ -1,6 +1,23 @@
-from tkinter import *
+from tkinter import Tk, Label, Button
 
-class Window(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
+class MyFirstGUI:
+    def __init__(self, master):
         self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    #Handler function to greet
+    def greet(self):
+        print("Greetings!")
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
