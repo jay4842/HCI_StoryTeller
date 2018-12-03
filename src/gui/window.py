5d90a2aa5c3e28305c5f10ffd91666bcebea3572
Ignacio Marroquin <ignacio9295@gmail.com>
2018-12-02 21:20:54 -0600

    Minor changes
    Added the author submenu

--- src/gui/window.py
+++ src/gui/window.py
@@ -1,25 +1,29 @@
 from tkinter import *
 
-class MyFirstGUI:
+class WindowGUI:
     def __init__(self, master):
         self.master = master
         master.title("HCI Project")
-        #e = Entry(master)
 
+        #Title
         self.label = Label(master, text="Menu")
-        self.label.grid(row=0)
-
+        self.label.grid(row=0, column=1)
+        
+        #Run Example Button
         self.greet_button = Button(master, text="Run Example", command=self.Run_Example)
-        self.greet_button.grid(row=1)
+        self.greet_button.grid(row=1, column=0)
 
+        #Author Button
         self.greet_button = Button(master, text="Author", command=self.Author)
-        self.greet_button.grid(row=2)
+        self.greet_button.grid(row=1, column=1)
 
+        #Image input Button
         self.greet_button = Button(master, text="Image Input", command=self.Img_Input)
-        self.greet_button.grid(row=3)
+        self.greet_button.grid(row=1, column=2)
 
+        #Quit Button
         self.close_button = Button(master, text="Quit", command=master.quit)
-        self.close_button.grid(row=4)
+        self.close_button.grid(row=2, column=1)
 
     #Handler function to greet
     def Run_Example(self):
@@ -27,11 +31,18 @@ class MyFirstGUI:
 
     def Author(self):
         print("Author Selected")
-        e.pack()
+        self.eTop = Toplevel()    #object to create a new window
+        self.eTop.title("Author")
+        self.e = Entry(self.eTop).grid(row=0, column=1)
+        self.get = Button(self.eTop, text = "Get Text", command=self.getText)
+        self.quit = Button(self.eTop, text = "Quit", command=self.eTop.destroy)
 
     def Img_Input(self):
         print("Image Input Selected")
 
+    def getText(self):
+        self.e.get()
+
 root = Tk()
-my_gui = MyFirstGUI(root)
+my_gui = WindowGUI(root)
 root.mainloop()
